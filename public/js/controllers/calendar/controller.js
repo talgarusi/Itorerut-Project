6ef724d94ee01552ffc03dc5e5d5b89d5a8f1a97
moshena <moshikoabc@gmail.com>
2016-05-15 14:06:19 +0300

    give more functionality and new desgin still have some bugs

--- public/js/controllers/calendar/controller.js
+++ public/js/controllers/calendar/controller.js
@@ -1,70 +1,93 @@
 var date = new Date();
 
-var monthArray = getDaysArray(date.getYear(),date.getMonth());
+var week;
+
+var monthArray;
+
+var miniString;
+
+var boxColorArray;
+
+Init(date);
+
+InitMonth(date);
 
 var normal=0;//offset to the calendar array.help us to know how many elemnts on the array are not real
 
 
 angular.module('contactsApp')
-	/* 
-        controller of the calendar
-    */
-    .controller('calendarCtrl' ,['$scope', function($scope){
-        $scope.days = monthArray;
-        $scope.numMonth = date.getMonth();
-        $scope.numMonth++;
-        $scope.month = getMonthName($scope.numMonth);
-        $scope.year = date.getFullYear();
-        
-        // click listner to add tasks
-        $scope.box$index=function() {
-        }
-        
-        
-        //add function to the buyyon last and next month
-        $scope.boxClicked=function(index) {
-            var i=0;
-            for(;$scope.days[i]==="";i++);
-            
-            index -= i;
-            index++;
-            if(index<1) return;
-        }
-        
-        $scope.ButtonNextMonthClick = function() {
-            $scope.numMonth++;
 
-            if($scope.numMonth >12)
-                {
-                    $scope.numMonth =1;
-                    $scope.year++;                    
-                }
-            $scope.month = getMonthName($scope.numMonth);
-            $scope.days = getDaysArray($scope.year,$scope.numMonth);
+    .controller('calendarCtrl' ,['$scope', function($scope){
+
+            update($scope,null);
+        
+        
+    
+
+
+        $scope.ButtonNextMonthClick = function() {
+            
+            date.setDate(date.getDate() + 7);
+            
+            update($scope,date);
 
         }
         
         $scope.ButtonbackMonthClick= function() {
-            $scope.numMonth--;
-
-            if($scope.numMonth < 1)
-                {
-                    $scope.numMonth =12;
-                    $scope.year--;                    
-                }
             
-            $scope.month = getMonthName($scope.numMonth);
-            $scope.days = getDaysArray($scope.year,$scope.numMonth);
+            date.setDate(date.getDate() - 7);
+            
+            update($scope,date);
+
 
         }
-        
+        $scope.today= function() {
+            
+            date = new Date();
+            
+            update($scope,date);
+        }
+
      }]);
+
+function Init(date) {
+    
+    week = new Array(7);
+    
+    var numInWeek = date.getDay();
+    
+    for(var i=0 ; i<7; i++)
+        {
+            if(i<numInWeek)
+                {
+                var copiedDate = new Date(date.getTime());
+                    copiedDate.setDate(copiedDate.getDate() + i - numInWeek);
+                    week[i] = copiedDate;  
+
+
+                }
+            else if(i>numInWeek)
+                {
+                    
+                var copiedDate = new Date(date.getTime());
+                    copiedDate.setDate( copiedDate.getDate() + i - numInWeek);   
+                    
+                    week[i] = copiedDate;  
+
+                }
+            else
+                {
+                    week.push(date);
+                    
+                    week[i] = date;
+
+                }
+        }
+}
 
 
 function getDaysArray(year, month) {
-    var firstime = true;
-    var firstday;
-    var loopSize=6;
+    
     var numDaysInMonth, daysInWeek, daysIndex, index, i, l, daysArray;
     numDaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     if(year%4 == 0) numDaysInMonth[1] = 29;
@@ -74,29 +97,12 @@ function getDaysArray(year, month) {
     daysArray = [];
 
     for (i = 0, l = numDaysInMonth[month - 1]; i < l; i++) {
-        daysArray.push((i + 1) + '. ' + daysInWeek[index++]);
         
-        if(firstime){
-            firstime = false;
-            firstday = daysInWeek[index];
-        }
+        daysArray.push((i + 1));
         
         if (index == 7) index = 0;
     }
-    
-    if(firstday == "Monday") loopSize=0;
-    if(firstday == "Tuesday") loopSize=1;
-    if(firstday == "Wednesday") loopSize=2;
-    if(firstday == "Thursday") loopSize=3;
-    if(firstday == "Friday") loopSize=4;
-    if(firstday == "Saturday") loopSize=5;
-    
-    normal = loopSize;
-    
-    for(var k=0; k<loopSize;k++)
-        {
-            daysArray.unshift("");
-        }
+
     return daysArray;
 }
 
@@ -105,3 +111,92 @@ function getMonthName(monthNumber) { //1 = January
             'July', 'August', 'September', 'October', 'November', 'December' ];
         return monthNames[monthNumber - 1];
     }
+
+function dateString() {
+    
+    var strRet;
+    
+    
+    if(week[0].getMonth() == week[6].getMonth())
+        {
+            strRet = getMonthName(week[0].getMonth()+1) +" "+ week[0].getDate() + " - " + week[6].getDate() + " ,                            "+week[6].getFullYear();
+        }
+    else
+        {
+           strRet = getMonthName(week[0].getMonth()+1) + " - " + getMonthName(week[6].getMonth()+1)+" "+ week[0].getDate() +           " - " +week[6].getDate() +" , "+week[6].getFullYear();
+        }
+    
+    return strRet;
+
+    }
+
+//update the week calendar
+update= function( $scope , date ) {
+            
+            if(date != null)
+                {
+                Init(date);
+                InitMonth(date)
+                }
+
+            $scope.sunDate = week[0].getDate();
+            $scope.monDate = week[1].getDate();
+            $scope.tuesDate = week[2].getDate();
+            $scope.wedDate = week[3].getDate();
+            $scope.thDate = week[4].getDate();
+            $scope.friDate = week[5].getDate();
+            $scope.satDate = week[6].getDate();
+
+            console.log(" before");
+            $scope.dateString = dateString();
+            $scope.monthArray= monthArray;
+            $scope.miniString = miniString;
+            $scope.boxColorArray = boxColorArray;
+            console.log(miniString);
+
+
+        }
+
+function InitMonth(date) {
+        
+    date = new Date(date.getFullYear(), date.getMonth(), 1);
+        
+    miniString = getMonthName(date.getMonth()+1) + " " + date.getFullYear();
+    
+    monthArray = new Array(42);
+    
+    boxColorArray = new Array(42);
+    
+    var afterMonth = false;
+    
+    var numInWeek = date.getDay();
+    
+    if(numInWeek ==0)
+        numInWeek =7;
+    
+    for(var i=0 ; i<42; i++)
+        {
+            
+            var copiedDate = new Date(date.getTime());
+                copiedDate.setDate(copiedDate.getDate() + i - numInWeek);
+                                
+                monthArray[i] = copiedDate;
+                //monthArray[i].marked= false;// need to find event in this day
+                boxColorArray[i] = "";
+            
+            if(i<numInWeek)
+                {
+                    boxColorArray[i] = "grey";
+                }
+            else if(monthArray[i-1].getMonth() < monthArray[i].getMonth() && i>27  || afterMonth)
+                { 
+                    afterMonth = true;
+                     boxColorArray[i] = "grey";
+                }
+
+        }
+    
+    
+}
+
+
