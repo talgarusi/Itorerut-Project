a436a79fcc9267057a2b7e6f62f147325130ba1b
GalZaken <Gal.zaken@gmail.com>
2016-05-31 00:30:04 +0300

    Fixed Tasks Statistics Percent Bug

--- public/js/controllers/dashboard/controller.js
+++ public/js/controllers/dashboard/controller.js
@@ -1,62 +1,58 @@
-angular.module('contactsApp')
-	/* 
-        controller of the home page
-    */
-    .controller('dashCtrl' ,['$scope', '$http', function($scope, $http){
-        /*
-        */ 
+/* DASHBOARD CONTROLLER: */
+angular.module('contactsApp').controller('dashCtrl' ,['$scope', '$http', function($scope, $http) {
+        
+        // SCOPES:
         $scope.h = window.innerHeight-100 +'px';
         $scope.lists = [];
         $scope.contacts = [];
         $scope.events = [];
-        
+
+        // GET TASKS LISTS FROM DB:
         $http.get("/listsDB").
         then(function(response) {
-                $scope.lists = response.data;
-            }, 
-            function(response) {
-                console.log("Error retrieving lists.");
+            $scope.lists = response.data;
+        }, 
+             function(response) {
+            console.log("Error retrieving lists.");
         });  
-        
+
+        // GET CONTACTS FROM DB:
         $http.get("/contactsDB").
         then(function(response) {
-                $scope.contacts = response.data;
-            }, 
-            function(response) {
-                console.log("Error retrieving contacts.");
+            $scope.contacts = response.data;
+        }, 
+             function(response) {
+            console.log("Error retrieving contacts.");
         });
-        
+
+        // GET EVENTS FROM DB:
         $http.get("/eventsDB").
         then(function(response) {
-                $scope.events = response.data;
-            }, 
-            function(response) {
-                console.log("Error retrieving events.");
+            $scope.events = response.data;
+        }, 
+             function(response) {
+            console.log("Error retrieving events.");
         });
-        
+
         // FUNCTIONS FOR TASK STATISTICS BOX:
-        var listIsComplete = function(list) {
-            
-            for (var i=0; i<list.tasks.length; i++)
-                if (!list.tasks[i].status)
-                    return false;
-            return true;
-        };
-        
-        $scope.getTasksStatistics = function() {
-            
+        $scope.tasksStatistics = function() {
+
             var completedTasksCounter = 0;
+            var listIsComplete = function(list) {
+
+                for (var i=0; i<list.tasks.length; i++)
+                    if (!list.tasks[i].status)
+                        return false;
+                return true;
+            };
+
             for (var i=0; i<$scope.lists.length; i++) {
-                
+
                 if (listIsComplete($scope.lists[i]))
                     completedTasksCounter++;
             }
-            
-            return (completedTasksCounter/$scope.lists.length)*100;
+
+            return (completedTasksCounter/$scope.lists.length*100).toFixed(2);
         };
-        
-        
-        
-        
-        
-     }]);  
+
+    }]); 
